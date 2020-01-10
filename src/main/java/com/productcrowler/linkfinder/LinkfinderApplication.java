70603244b4288dd74400123ce53d4b7b0f5d68b0
sasmit rashinkar <sasmit.rashinkar@darkmatter.ae>
2020-01-11 00:40:34 +0530

    intial commit

---
+++ src/main/java/com/productcrowler/linkfinder/LinkfinderApplication.java
@@ -0,0 +1,13 @@
+package com.productcrowler.linkfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkfinderApplication.class, args);
+	}
+
+}
